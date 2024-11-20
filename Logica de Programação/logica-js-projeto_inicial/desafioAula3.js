7ffa473567e5e291aa523dde9826dbc42e5d9f54
EnzoCavalcanti <enzo.cavalcanti15@gmail.com>
2024-11-20 20:17:02 -0300

    Create desafioAula3.js

---
+++ Logica de Programação/logica-js-projeto_inicial/desafioAula3.js
@@ -0,0 +1,28 @@
+//Desafio 1
+let contador1 = 1;
+while(contador <= 10 ){
+    console.log(contador);
+    contador++;
+};
+
+//Desafio 2
+let contador2 = 10;
+while(contador2 >= 0){
+    console.log(contador2);
+    contador2--;
+}
+
+//Desafio 3
+let contagemR = prompt('Informe um numero');
+while(contagemR >= 0){
+    console.log(contagemR);
+    contagemR--;
+}
+
+//Desafio 4
+let contagemP = prompt('Informe um numero');
+let i = 0;
+while(contagemP >= i){
+    console.log(contagemP);
+    i++;
+}
