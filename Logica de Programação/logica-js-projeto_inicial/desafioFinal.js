089b11648c6cbcba6c1b3887a23d860977da95d0
EnzoCavalcanti <enzo.cavalcanti15@gmail.com>
2024-11-20 20:17:20 -0300

    Add files via upload

---
+++ Logica de Programação/logica-js-projeto_inicial/desafioFinal.js
@@ -0,0 +1,71 @@
+//Desafio 1
+console.log('Boas vindas!');
+
+//Desafio 2
+let nome = Enzo;
+console.log(`Olá,${nome}!`);
+
+//Desafio 3
+let nome3 = Enzo;
+alert(`Olá,${nome3}!`);
+
+//Desafio 4
+let linguagem = prompt('Qual a linguagem de programação que você mais gosta?');
+console.log(linguagem);
+
+//Desafio 5
+let valor1 = 24;
+let valor2 = 36;
+let resultado1 = valor1 + valor2;
+console.log(`A soma entre ${valor1} e ${valor2} é igual a ${resultado1}.`);
+
+//Desafio 6
+let valor3 = 64;
+let valor4 = 34;
+let resultado2 = valor3 - valor4;
+console.log(`A diferença entre ${valor3} e ${valor4} é igual a ${resultado2}.`);
+
+//Desafio 7 
+let idade = prompt('Qual a sua idade ?');
+if(idade >= 18 ){
+    console.log('Voce é maior de idade!');
+} else {
+    console.log('Voce é menor de idade!');
+}
+
+//Desafio 8
+let numero = prompt('Informe um valor positivo ou negativo.');
+if(numero > o){
+    console.log('O número é positivo');
+} else if (numero < 0){
+    console.log('O número é negativo');
+} else {
+    console.log('O número é zero');
+}
+
+//Desafio 9
+let i = 1;
+while(i<=10){
+    console.log(i);
+    i++;
+}
+
+//Desafio 10
+let nota = 9;
+if(nota >= 7){
+    console.log('Aprovado');
+} else {
+    console.log('Reprovado');
+}
+
+//Desafio 11
+let num = Math.random();
+console.log(num);
+
+//Desafio 12
+let numeroA = parseInt(Math.random()* 10 + 1);
+console.log(numeroA);
+
+//Desafio 13
+let numeroAleatorio = parseInt(Math.random()* 1000 + 1);
+console.log(numeroAleatorio);
